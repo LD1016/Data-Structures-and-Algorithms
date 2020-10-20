d21dc01fe4a0c40b1656b5ee79f3d6962b891ba5
Lance Dang <lancedang@Lances-MacBook-Pro.local>
2020-10-19 19:42:46 -0700

    Tree from scratch and Heap library in python

---
+++ Trees/Tree.py
@@ -0,0 +1,66 @@
+class TreeNode: 
+    def __init__(self, data):
+        self.left = None 
+        self.right = None
+        self.value = data 
+    
+class Tree: 
+    def __init__(self):
+        self.root = None 
+
+    def insert(self, data):
+        newNode = TreeNode(data)
+        if self.root is None: 
+            self.root = newNode 
+            return 
+        currNode = self.root 
+        while True: 
+            # Check if newNode should go to leftside
+            if currNode.value > data: 
+                if currNode.left is None:
+                    currNode.left = newNode
+                    return 
+                else: 
+                    currNode = currNode.left 
+            elif currNode.value < data:
+                if currNode.right is None:
+                    currNode.right = newNode
+                    return 
+                else: 
+                    currNode = currNode.right
+                
+    def lookup(self, data):
+        if self.root is None: 
+            return False 
+        currNode = self.root 
+        while currNode is not None:
+            if data == currNode.value:
+                return True 
+            elif data < currNode.value:
+                currNode = currNode.left 
+            elif data > currNode.value:
+                currNode = currNode.right 
+        return False
+
+    def print_tree(self):
+        if self.root != None:
+            self.printt(self.root)
+#Inorder Traversal (We get sorted order of elements in tree)
+
+    def printt(self,curr_node):
+        if curr_node != None:
+            self.printt(curr_node.left)
+            print(str(curr_node.value))
+            self.printt(curr_node.right)
+
+bst = Tree()
+bst.insert(10)
+bst.insert(5)
+bst.insert(6)
+bst.insert(12)
+bst.insert(8)
+x = bst.lookup(6)
+print(x)
+y = bst.lookup(99)
+print(y)
+bst.print_tree()
